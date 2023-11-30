650a9d244308e9670daeea93b9e710763364d1e1
Jeff Maruli <51332987+xietsunzao@users.noreply.github.com>
2023-12-01 03:29:37 +0700

    fix: :recycle: add method for redirection short url

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const app = express();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
+const urlMappings = new Map();
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -21,8 +22,21 @@ app.post('/api/shorturl', function (req, res) {
     }
 
     const shortUrl = Math.floor(Math.random() * 100000).toString();
+    urlMappings.set(shortUrl, url);
 
     res.json({ original_url: url, short_url: shortUrl });
+});
+
+app.get('/api/shorturl/:short_url', (req, res) => {
+    const { short_url } = req.params;
+
+    // Check if the short URL exists in the mapping
+    if (urlMappings.has(short_url)) {
+        // Redirect to the original URL
+        res.redirect(urlMappings.get(short_url));
+    } else {
+        res.json({ error: 'short URL not found' });
+    }
 });
 
 app.listen(port, function () {
